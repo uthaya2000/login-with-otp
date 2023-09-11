4454640c5438a96921a7f0e95fa8688d74975d5f
uthaya_n2000 <uthaya.sankar@mckinleyrice.co>
2023-09-11 13:58:35 +0530

    Initial Commit

---
+++ src/main/java/com/us/worlddetails/WorldDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.us.worlddetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldDetailsApplication.class, args);
+	}
+
+}
